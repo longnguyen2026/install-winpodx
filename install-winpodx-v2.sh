355f9991a668cea69b81063705a902dd451d8d5d
nam2026 <cloud07622@gmail.com>
2026-07-28 21:29:07 +0700

    Update install-winpodx-v2.sh

--- install-winpodx-v2.sh
+++ install-winpodx-v2.sh
@@ -281,7 +281,7 @@ echo
 
 section "Installing WinPodX"
 
-curl -fsSL https://raw.githubusercontent.com/kernalix7/winpodx/main/install.sh | bash
+curl -fsSL https://raw.githubusercontent.com/kernalix7/winpodx/main/install-v2.sh | bash
 
 unset WINPODX_WIN_VERSION
 
