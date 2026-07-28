0dc5d22355ca3e9033913724649e2d47f8473b55
nam2026 <cloud07622@gmail.com>
2026-07-28 20:32:52 +0700

    Create install-v2.sh

---
+++ install-v2.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO="https://raw.githubusercontent.com/longnguyen2026/install-winpodx/main"
+SCRIPT="install-winpodx-v2.sh"
+
+TMP="/tmp/$SCRIPT"
+
+echo "Downloading WinPodX installer..."
+
+curl -fsSL "$REPO/$SCRIPT" -o "$TMP"
+
+chmod +x "$TMP"
+
+exec bash "$TMP"
